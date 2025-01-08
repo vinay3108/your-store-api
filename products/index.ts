9c9722f4c510c4a1d871893748c28bb4e1c9c376
vinaykr3108 <vinaykr3108@gmail.com>
2025-01-09 00:06:53 +0530

    microservice | added docker file

---
+++ products/index.ts
@@ -0,0 +1,33 @@
+import express  from "express";
+require('module-alias/register');
+import { Request,Response } from "express";
+
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config()
+
+const port = process.env.PORT || 5006;
+app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+app.use(express.static("public"))
+
+const startApp = async () => {
+    try {
+        app.get('/',(req:Request ,res:Response)=>{
+            res.send("hello from products");
+        })
+        app.get('/product',(req:Request,res:Response)=>{
+            res.send("vinay");
+        })
+        app.listen(port,()=>{
+            console.log(`server running on port ${port}`);
+        })
+    } catch (err) {
+        console.error("Failed to start application due to DB error:", err);
+        process.exit(1); // Exit the application on failure
+    }
+};
+
+startApp();

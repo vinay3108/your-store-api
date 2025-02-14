b00907ad2566f44f80c7f466dc81af97dfeacf8e
Banti Kumar <mr.bantikumar9716@gmail.com>
2025-02-14 23:12:58 +0530

    add dotenv or cors

--- products/index.ts
+++ products/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
+import cors from 'cors';
 require("module-alias/register");
 import { Request, Response } from "express";
 import ProductRouter from "@root/Routes/product.route";
 import dbConnection from "./DB/db.connection";
-
 import dotenv from "dotenv";
-
-const app = express();
 
 dotenv.config();
 
+const app = express();
+
 const port = process.env.PORT || 5007;
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
